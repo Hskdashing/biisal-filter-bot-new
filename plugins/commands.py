30156c55f169d47dca66f8eaa1e84363550af800
Hsk <149724465+Hskdashing@users.noreply.github.com>
2024-08-30 01:47:23 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -808,7 +808,7 @@ async def set_shortner_3(c, m):
     try:
         URL = m.command[1]
         API = m.command[2]
-        resp = requests.get(f'https://{URL}/api?api={API}&url=https://telegram.dog/bisal_files').json()
+        resp = requests.get(f'https://{URL}/api?api={API}&url=https://telegram.dog/zballupdatws').json()
         if resp['status'] == 'success':
             SHORT_LINK = resp['shortenedUrl']
         await save_group_settings(grp_id, 'shortner_three', URL)
